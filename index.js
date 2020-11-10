239686e6eaf418aa5912aca5d05fa814d5f00f22
jagadale_commits <jagadale.new@gmail.com>
2020-11-10 08:29:59 +0530

    deployment changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
-
+const PORT = process.env.port || 5000
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -17,8 +17,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.log(err)
   });
